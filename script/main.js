f7dc3eb106daef6213e1c3980b409ea848a24062
Cholponai Manapova <111632987+Cholponai02@users.noreply.github.com>
2026-01-28 23:25:48 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,13 +1,13 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
+        title: 'Хочешь посмотреть ?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
+        confirmButtonText: 'Конечно',
+        cancelButtonText: 'Да',
     }).then((result) => {
         if (result.isConfirmed) {
             document.querySelector('.song').play();
